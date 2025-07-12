5f34acb0eab2ddfb1380a4c259070de3a6a62c1f
Ansah Hamzat Johnson <jansah.klintaps@gmail.com>
2025-07-12 20:53:50 +0000

    Correction on file creation

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// 0-javascript_is_amazing.js
+
+// Create a constant variable named myVar and assign it the string value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value stored in myVar to the console
+console.log(myVar);
